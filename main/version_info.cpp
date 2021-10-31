c1f4f5c17d6e74279dbb52f923caa557a3a7219f
OpenDeltaImage <86120617+OpenDeltaImage@users.noreply.github.com>
2021-10-31 02:36:52 +0100

    Update version_info.cpp

--- main/version_info.cpp
+++ main/version_info.cpp
@@ -9,7 +9,7 @@ const char *enigma2_date = ENIGMA2_COMMIT_DATE;
 #ifndef ENIGMA2_BRANCH
 #define ENIGMA2_BRANCH "(no branch)"
 #endif
-#const char *enigma2_branch = ENIGMA2_BRANCH;
+const char *enigma2_branch = ENIGMA2_BRANCH;
 
 #ifndef ENIGMA2_REV
 #define ENIGMA2_REV ""
